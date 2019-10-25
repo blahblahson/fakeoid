e03198d7f0932b3327c244be16ddfd082874663f
Alvin Šipraga <alvin@pqrs.dk>
2019-10-25 23:07:58 +0200

    Initial version

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode
+  (flycheck-clang-language-standard . "c11")
+  (flycheck-gcc-language-standard . "c11")))
+
+
